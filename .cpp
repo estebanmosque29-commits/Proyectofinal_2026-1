bd729b5505708d7027cc9b129d4519326a901b9d
Esteban Mosquera <estebanmosque29@gmail.com>
2026-05-27 07:34:44 -0500

    Update .cpp

--- .cpp
+++ .cpp
@@ -1,4 +1,11 @@
-int main() {
+// main.cpp
+#include <QApplication>
+#include "mainwindow.h"
 
-    return 0;
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
